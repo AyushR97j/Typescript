21f7b7935d663dbcb8c076ad56c446f8deb62911
AyushR97j <ayushraj803110@gmail.com>
2024-05-06 23:30:50 +0530

    Function param type & return type

--- a.js
+++ a.js
@@ -1,3 +1,21 @@
 "use strict";
 let x = 45;
 console.log(x);
+//error/////////////////////////////////////////////                       
+/*
+x = "hello";
+console.log(x);
+*/
+////////////////////////////////////////////////////                       
+//Functions argumets////////////////////////////////                       
+function greet(firstName, lastName) {
+    console.log("Hello " + firstName, lastName);
+}
+greet("Ayush", "Raj");
+////////////////////////////////////////////////////                       
+//sum function - return type////////////////////////
+function sum(a, b) {
+    return a + b;
+}
+const value = sum(4, 5);
+console.log(value);
--- a.ts
+++ a.ts
@@ -1,2 +1,23 @@
 let x: number = 45;
 console.log(x);
+
+//error/////////////////////////////////////////////                       
+/*
+x = "hello";
+console.log(x);
+*/
+////////////////////////////////////////////////////                       
+
+//Functions argumets////////////////////////////////                       
+function greet(firstName: string, lastName: string) {
+    console.log("Hello "+ firstName, lastName);
+}
+greet("Ayush", "Raj");
+////////////////////////////////////////////////////                       
+
+//sum function - return type////////////////////////
+function sum(a: number, b: number): number {
+    return a + b;
+}
+const value = sum(4, 5);
+console.log(value);
